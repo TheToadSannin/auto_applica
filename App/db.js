df905cdd718b8300622bb8d125501003dd9636b0
rudrasama <luckybairagi96@gmail.com>
2023-07-18 19:33:40 +0530

    Server

---
+++ App/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const mongoUri =
+  "mongodb+srv://gauxrav:14aca70dc47e69g@cluster0.lctxgaw.mongodb.net/auto_applica";
+
+const mongoDB = async () => {
+  await mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log("connected");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = mongoDB;
---
+++ App/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const mongoDB = require('./db');
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.get('/', function(req, res){
+    res.send("hello");
+});
+
+
+mongoDB();
+app.listen(5000, ()=>{
+    console.log("server is running at port 5000");
+});
