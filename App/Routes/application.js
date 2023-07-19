908585d02faa936a2f78a7d1f44345e82d2c63c8
Gaurav panwar <gp771234@gmail.com>
2023-07-19 22:34:52 +0530

    sdf

--- App/Routes/application.js
+++ App/Routes/application.js
@@ -3,16 +3,6 @@ const Application = require("../models/Applications");
 
 const router = express.Router();
 
-const student1 = {
-  fullname: "sdf;lk",
-  roll_no: "alsfk",
-  email: "aslfjasl@gmail.com",
-  standard: "a",
-  section: " a",
-  address: " asd;lfkjasd;lfjsd;lfjdslj",
-  password: "as;lfjslfjldfj",
-};
-
 router.post(
   "/createApplication",
 
@@ -27,7 +17,7 @@ router.post(
           section: " a",
           address: " asd;lfkjasd;lfjsd;lfjdslj",
           password: "as;lfjslfjldfj",
-        }
+        },
       });
     } catch (error) {
       res.json(error);
