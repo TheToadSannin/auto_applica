ad911d1798a22ad8ce75a3c40e524151a00ee630
Gaurav panwar <gp771234@gmail.com>
2023-07-25 21:23:54 +0530

    providers done

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,26 +1,27 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-import AuthContext from './AuthContext';
+import AuthContext from "./AuthContext";
 
-const getStudent = async(e)=>{
-    e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/isAuth", {
-        method: "GET",
-        headers: {
-            "Content-Type":"application/json",
-            "x-access-token": localStorage.getItem("token")
-        }
-    });
-    return await response.json();
-}
-
-export const AuthProvider = ({children}) =>{
-    const {student, setStudent} = useState(null);
-    useEffect(()=>{
-        const currentStudent = getStudent();
-        setStudent(currentStudent);
-    }, []);
-
-    return (<AuthContext.Provider value={{student}}>{children}</AuthContext.Provider>)
-
+const getStudent = async (e) => {
+  const response = await fetch("http://localhost:5000/api/isAuth", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token"),
+    },
+  });
+  return await response.json();
 };
+
+export const AuthProvider = ({ children }) => {
+  const [student, setStudent] = useState(null);
+  useEffect(() => {
+    const currentStudent = getStudent();
+    setStudent(currentStudent);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ student }}>{children}</AuthContext.Provider>
+  );
+};
+export default AuthProvider;
--- src/providers/useAuthContext.js
+++ src/providers/useAuthContext.js
@@ -1,10 +1,12 @@
 import AuthContext from "./AuthContext";
+import { useContext } from "react";
 
-const useAuthContext = () =>{
-    const student = useContext(AuthContext);
-    if(student === undefined){
-        throw new Error("useAuthContext can only be used inside AuthProvider");
-    }
-    return student;
-}
+const useAuthContext = () => {
+  const student = useContext(AuthContext);
+  if (student === undefined) {
+    throw new Error("useAuthContext can only be used inside AuthProvider");
+  }
+  return student;
+};
 
+export default useAuthContext;
