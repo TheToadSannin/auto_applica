b648d9c2083d0d61a7be90fdd9eebc04ee858de2
rudrasama <luckybairagi96@gmail.com>
2023-07-26 01:37:01 +0530

    again to providers

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -1,8 +1,4 @@
 import { createContext } from "react"; 
 
-<<<<<<< HEAD
-export const AuthContext = createContext(null);;
-=======
 const AuthContext = createContext();
-module.exports = AuthContext;
->>>>>>> parent of ad911d1 (providers done)
+export default AuthContext;
--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect } from "react";
-import { AuthContext } from "./AuthContext";
-const getStudent = async () => {
-  const response = await fetch("http://localhost:5000/api/isAuth", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "x-access-token": localStorage.getItem("token"),
-    },
-  });
-  
-    const json = await response.json();
-    return json.student;
-};
-
-
-
-export const AuthProvider = ({ children }) => {
-  const [student, setStudent] = useState(null);
-  useEffect(() => {
-    getStudent().then((response)=>{
-        const currentStudent = response;
-        setStudent(currentStudent);  
-    })
-  }, []);
-
-  return (
-    <AuthContext.Provider value={ {student} }>{children}</AuthContext.Provider>
-  );
-};
-export default AuthProvider;
-=======
 import {useState, useEffect} from 'react';
 
 import AuthContext from './AuthContext';
@@ -57,4 +24,3 @@ export const AuthProvider = ({children}) =>{
     return (<AuthContext.Provider value={{student}}>{children}</AuthContext.Provider>)
 
 };
->>>>>>> parent of ad911d1 (providers done)
