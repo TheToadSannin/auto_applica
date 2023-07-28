c765ccb08ebd080f91e6f37562693938b7f57496
Gaurav panwar <gp771234@gmail.com>
2023-07-28 20:59:33 +0530

    navbar design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {ReactComponent as Logo} from "../assets/logo.svg";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <span><Logo height={120+"px"} width={120+"px"} viewBox={"0 0 500 300"}/></span>
-        <ul>
-          <li><Link to='/'>Home </Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/login'>Login</Link></li>
-          <li><div className='logo'>L</div></li>
+        <span>
+          <Logo
+            height={120 + "px"}
+            width={120 + "px"}
+            viewBox={"0 0 500 300"}
+          />
+        </span>
+        <ul className="pages">
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <ul className="actions">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/singup">Singup</Link>
+          </li>
+          <li>
+            <div className="logo">L</div>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-
-
-export default Header
+export default Header;
