33b800cdd1780f4d7b0c6701862f981a544f29ae
rudrasama <luckybairagi96@gmail.com>
2023-07-31 02:08:23 +0530

    Hmm

--- src/pages/students/CreateApplication.jsx
+++ src/pages/students/CreateApplication.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 
 const CreateApplication = () => {
   return (
-    <main> 
-      <div>
-      
-
+    <main className='applicationMain'>
+      <div className='cards'>
+        <AppCard />
       </div>
     </main>
+  )
+}
+
+const AppCard = () => {
+  return (
+    <div className='card'>
+      <div>
+        <img src="https://picsum.photos/300/300" alt="asdfasdf" />
+      </div>
+      <div className='title-div'>
+        <h1>Title</h1>
+        <div className='content'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim praesentium eveniet tenetur nesciunt officiis eius id, repudiandae soluta cupiditate nemo!</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
