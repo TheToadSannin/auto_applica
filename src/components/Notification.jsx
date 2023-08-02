54e1b4afda00d89711d4c03864bfeb6b47d9b369
Gaurav panwar <gp771234@gmail.com>
2023-08-02 13:43:46 +0530

    alert timer

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,6 +4,7 @@ const Notification = (props) => {
   return (
     <main className="notification">
       <div>{props.message}</div>
+      <div class="timer"></div>
     </main>
   );
 };
--- src/pages/teachers/Dashboard.jsx
+++ src/pages/teachers/Dashboard.jsx
@@ -1,67 +1,76 @@
-import React from 'react'
-import { useContext } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AuthContext from '../../providers/AuthContext';
+import React from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../providers/AuthContext";
 
 const Dashboard = () => {
+  const [applications, setApplications] = useState();
+  const { user, role, isLoading, authenticated } = useContext(AuthContext);
 
-    const [applications, setApplications] = useState();
-    const { user, role, isLoading, authenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoading) {
+      if (!authenticated || role != "teacher") {
+        navigate("/login");
+      }
+    }
+  }, [isLoading, authenticated]);
 
-
-
-    useEffect(() => {
-        if (!isLoading) {
-            if (!authenticated || role != "teacher") {
-                navigate("/login");
-            }
+  useEffect(() => {
+    const getApplication = async () => {
+      const standard = user.standard;
+      const response = await fetch(
+        `http://localhost:5000/api/getApplicationWithStudent?standard=${standard}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    }, [isLoading, authenticated])
+      );
 
+      const json = await response.json();
+      setApplications(json);
+    };
+    if (!isLoading) {
+      getApplication();
+    }
+  }, [isLoading]);
 
+  return (
+    <main className="dashboard">
+      {applications
+        ? applications.map((application, index) => {
+            return (
+              <AppCard
+                key={index}
+                title={application.title}
+                date={application.timestamp}
+                status={application.isAccepted}
+              />
+            );
+          })
+        : ""}
+    </main>
+  );
+};
 
-    useEffect(() => {
-        const getApplication = async () => {
-            const standard = user.standard;
-            const response = await fetch(`http://localhost:5000/api/getApplicationWithStudent?standard=${standard}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+const AppCard = (props) => {
+  return (
+    <div className="appcard">
+      <div>
+        <p>{props.title}</p>
+      </div>
+      <hr />
+      <div>
+        <p>Status: {props.isAccepted ? "Accepted" : "Pending"}</p>
+        <p> Submission Date: {props.date}</p>
+      </div>
+    </div>
+  );
+};
 
-            const json = await response.json();
-            setApplications(json);
-        }
-        if (!isLoading) {
-            getApplication();
-        }
-
-
-    }, [isLoading]);
-
-
-
-
-
-    return (
-        <div>
-            {
-                (applications) ? ((applications.length > 0) ? (
-                    applications.map((application, index) => {
-                        return <p key={index}>{application.title + " by " + application.student.fullname}</p>
-                    })
-                ) : ("No Applications")) : ""
-            }
-            {
-                // console.log(user.name)
-            }
-        </div>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
